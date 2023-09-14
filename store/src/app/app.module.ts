1a0d6f6301e3d53cfa54be8161af6dac9b4b13c0
Daniel Novais <danlicesol@gmail.com>
2023-09-14 03:32:23 -0300

    estilizando

--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { CardComponent } from './components/card/card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { CardLabelComponent } from './components/card/card-label/card-label.component';
 import { CardPriceComponent } from './components/card/card-price/card-price.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { SearchComponent } from './components/menu-bar/search/search.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { CardPriceComponent } from './components/card/card-price/card-price.comp
     CardComponent,
     MenuBarComponent,
     CardLabelComponent,
-    CardPriceComponent
+    CardPriceComponent,
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
